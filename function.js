da451e6c4eaf89f59e9ec8436bc6a680ea4229ea
Aran Ansari <amgansari@gmail.com>
2023-06-29 16:28:59 -0400

    Add new fullpage portfolio site

--- function.js
+++ function.js
@@ -55,79 +55,34 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
-function getAge(dateString) {
+  function getAge(dateString) {
     var now = new Date();
-    var today = new Date(now.getYear(),now.getMonth(),now.getDate());
-  
-    var yearNow = now.getYear();
-    var monthNow = now.getMonth();
-    var dateNow = now.getDate();
-  
-    var dob = new Date('05/04/1997');
-  
-    var yearDob = dob.getYear();
-    var monthDob = dob.getMonth();
-    var dateDob = dob.getDate();
-    var age = {};
-    var ageString = "";
-    var yearString = "";
-    var monthString = "";
-    var dayString = "";
-  
-  
-    yearAge = yearNow - yearDob;
-  
-    if (monthNow >= monthDob)
-      var monthAge = monthNow - monthDob;
-    else {
-      yearAge--;
-      var monthAge = 12 + monthNow -monthDob;
+    var dob = new Date(dateString);
+    
+    var years = now.getFullYear() - dob.getFullYear();
+    var months = now.getMonth() - dob.getMonth();
+    var days = now.getDate() - dob.getDate();
+    var hours = now.getHours() - dob.getHours();
+
+    if (hours < 0) {
+      hours += 24;
+      days--;
     }
-  
-    if (dateNow >= dateDob)
-      var dateAge = dateNow - dateDob;
-    else {
-      monthAge--;
-      var dateAge = 31 + dateNow - dateDob;
-  
-      if (monthAge < 0) {
-        monthAge = 11;
-        yearAge--;
-      }
+    if (days < 0) {
+      months--;
+      days += new Date(now.getFullYear(), now.getMonth(), 0).getDate();
     }
-  
-    age = {
-        years: yearAge,
-        months: monthAge,
-        days: dateAge
-        };
-  
-    if ( age.years > 1 ) yearString = " years";
-    else yearString = " year";
-    if ( age.months> 1 ) monthString = " months";
-    else monthString = " month";
-    if ( age.days > 1 ) dayString = " days";
-    else dayString = " day";
-  
-  
-    if ( (age.years > 0) && (age.months > 0) && (age.days > 0) )
-      ageString = age.years + yearString + ", " + age.months + monthString + ", and " + age.days + dayString + " old.";
-    else if ( (age.years == 0) && (age.months == 0) && (age.days > 0) )
-      ageString = "Only " + age.days + dayString + " old!";
-    else if ( (age.years > 0) && (age.months == 0) && (age.days == 0) )
-      ageString = age.years + yearString + " old. Happy Birthday Me!!";
-    else if ( (age.years > 0) && (age.months > 0) && (age.days == 0) )
-      ageString = age.years + yearString + " and " + age.months + monthString + " old.";
-    else if ( (age.years == 0) && (age.months > 0) && (age.days > 0) )
-      ageString = age.months + monthString + " and " + age.days + dayString + " old.";
-    else if ( (age.years > 0) && (age.months == 0) && (age.days > 0) )
-      ageString = age.years + yearString + " and " + age.days + dayString + " old.";
-    else if ( (age.years == 0) && (age.months > 0) && (age.days == 0) )
-      ageString = age.months + monthString + " old.";
-    else ageString = "Oops! Could not calculate age!";
-  
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    var ageString = years + " years, " + months + " months, " + days + " days, and " + hours + " hours old.";
     return ageString;
   }
+
+  console.log(getAge('05/04/1997'));
+
   
   function fadeInOnScroll() {
     var fadeElements = document.querySelectorAll(".fade-in-scroll");
---
+++ main.js
@@ -0,0 +1,203 @@
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+  function getAge(dateString) {
+    var now = new Date();
+    var dob = new Date(dateString);
+    
+    var years = now.getFullYear() - dob.getFullYear();
+    var months = now.getMonth() - dob.getMonth();
+    var days = now.getDate() - dob.getDate();
+    var hours = now.getHours() - dob.getHours();
+
+    if (hours < 0) {
+      hours += 24;
+      days--;
+    }
+    if (days < 0) {
+      months--;
+      days += new Date(now.getFullYear(), now.getMonth(), 0).getDate();
+    }
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    var ageString = years + " years, " + months + " months, " + days + " days, and " + hours + " hours old.";
+    return ageString;
+  }
+
+  console.log(getAge('05/04/1997'));
+
+  
+  function fadeInOnScroll() {
+    var fadeElements = document.querySelectorAll(".fade-in-scroll");
+    
+    fadeElements.forEach(function(element) {
+      var elementPosition = element.getBoundingClientRect().top;
+      var windowHeight = window.innerHeight * 0.75;
+      
+      if (elementPosition < windowHeight) {
+        element.classList.add("show");
+      }
+    });
+  }
+  
+  window.addEventListener("scroll", fadeInOnScroll);
+  
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var fadeElements = document.querySelectorAll(".fade-in");
+    
+    fadeElements.forEach(function(element) {
+      element.classList.add("show");
+    });
+  });
+
+
+  new fullpage('#fullpage', {
+    controlArrows: false,
+    scrollOverflow:true,
+    verticalCentered: true,
+    scrollingSpeed: 600,
+    scrollHorizontally: true,
+    dragAndMove: true,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    anchors: ['home', 'work', 'projects', 'about', 'resume'],
+    menu: '.fixed-nav-bar',
+    onSlideLeave: function(section, origin, destination, direction){
+        let activeLink = document.querySelector('.fixed-nav-bar .active');
+        if (activeLink) {
+            activeLink.classList.remove('active');
+        }
+        let links = document.querySelectorAll('.fixed-nav-bar a');
+        let activeIndex = destination.index;
+        links[activeIndex].classList.add('active');
+    },
+    afterRender: function(){
+        // getting the anchor link for the current slide
+        var activeLink = document.querySelector('.fixed-nav-bar a[href="#' + window.location.hash.split('/')[1] + '"]');
+        if (activeLink) {
+            activeLink.classList.add('active');
+        }
+    },
+});
+
+
+
+/* 
+const decoration = document.querySelector('.decoration');
+
+
+  document.addEventListener('mousemove', function(e) {
+    let x = e.clientX; // Current X coordinate of the mouse pointer
+    let y = e.clientY; // Current Y coordinate of the mouse pointer
+  
+    document.querySelectorAll('.decoration img').forEach(function(image) {
+      let rect = image.getBoundingClientRect(); // Get the position of the image relative to the viewport
+      let imgX = rect.left + rect.width / 2; // X coordinate of the center of the image
+      let imgY = rect.top + rect.height / 2; // Y coordinate of the center of the image
+  
+      let dx = imgX - x; // Distance from the mouse pointer to the center of the image (X axis)
+      let dy = imgY - y; // Distance from the mouse pointer to the center of the image (Y axis)
+  
+      let depth = image.dataset.depth;
+      image.style.transform = `translate(${dx * depth}px, ${dy * depth}px)`;
+    });
+  });
+
+  var scene = document.getElementById('scene');
+  var parallaxInstance = new Parallax(scene);
+
+
+ */
+/* NEW ATTEMPT AT PARTICLE BACKGROUND */
+window.addEventListener('load', function() {
+    // Your 'window.onload' code here
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+});
+
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+ });
+ 
+
+ $(document).ready(function(){
+    $('.gallery').slick({
+        dots: true, // Add navigation dots
+        infinite: false, // Enable infinite scrolling
+        speed: 300, // Set animation speed (in milliseconds)
+        slidesToShow: 1, // Show one slide at a time
+        slidesToScroll: 1 // Scroll one slide at a time
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var currentLocation = window.location.href;
+    var homeLink = document.getElementById("home-link");
+    var links = document.querySelectorAll(".fixed-nav-bar a");
+    
+    links.forEach(function(link) {
+        if (link.href === currentLocation) {
+            link.classList.add("active");
+        }
+    });
+    
+    // If no link is found, default to the "HOME" link
+    if (!document.querySelector(".fixed-nav-bar .active")) {
+        homeLink.classList.add("active");
+    }
+});
+
